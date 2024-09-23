b82e88e1a15eabb55f0ebb655b7dca6bee22d249
ferreiracamilo <camiloferreira2009@hotmail.com>
2024-09-23 00:38:14 -0300

    update link refs

--- scripts/links.js
+++ scripts/links.js
@@ -1,5 +1,5 @@
-const baseURL = "https://ferreiracamilo.github.io/wdd230/";
-const linksURL = "https://ferreiracamilo.github.io/wdd230/data/links.json";
+const baseURL = "https://ferreiracamilo.github.io/wdd231/";
+const linksURL = "https://ferreiracamilo.github.io/wdd231/data/links.json";
 
 async function getLinks() {
     const response = await fetch(linksURL);
