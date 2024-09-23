c611aa89950c61bb45c0b0e52f56604028af4ba8
ferreiracamilo <camiloferreira2009@hotmail.com>
2024-09-23 02:02:25 -0300

    Show old courses js updated

--- scripts/showCourses.js
+++ scripts/showCourses.js
@@ -1,34 +1,56 @@
-const linksURL = "https://ferreiracamilo.github.io/wdd231/data/courses.json";
+const courseWorkURL = "https://ferreiracamilo.github.io/wdd231/data/courses.json";
 
 async function getCourses() {
-    const response = await fetch(linksURL);
+    const response = await fetch(courseWorkURL);
     const data = await response.json();
-    displayCourses(data.courses);
+    return data.courses;
 }
 
-// Función para mostrar la lista de cursos
 function displayCourses(courses) {
-    const courseTable = document.getElementById('courses-table');
+    const coursesTable = document.querySelector('#courses-table');
+    coursesTable.innerHTML = ''; //Clean courses table to show update content based on course filter button
 
-    // Recorrer cada curso y crear un elemento <div> para mostrarlo
+    // Show all courses from list received in argument
     courses.forEach(course => {
         const courseDiv = document.createElement('div');
         
-        // Asignar la clase según el valor de "completed"
+        // Assign class based on course.completed property set on json
         if (course.completed) {
             courseDiv.classList.add('course-completed');
         } else {
             courseDiv.classList.add('course-incomplete');
         }
 
-        // Contenido del div
         courseDiv.innerHTML = `
             <strong>${course.subject} ${course.number}</strong>
         `;
-        
-        // Añadir el div al div principal
-        courseTable.appendChild(courseDiv);
+
+        coursesTable.appendChild(courseDiv);
     });
 }
 
-getCourses();
+// Filter courses based on subject
+function filterCourses(subject, allCourses) {
+    if (subject === 'all') {
+        return allCourses;
+    }
+    return allCourses.filter(course => course.subject === subject);
+}
+
+// Handle course filters
+async function setupFilters() {
+    const allCourses = await getCourses(); // load all courses from data json
+
+    document.querySelectorAll('.filter-button').forEach(button => {
+        button.addEventListener('click', (event) => {
+            const filterValue = event.target.getAttribute('data-filter');
+            const filteredCourses = filterCourses(filterValue, allCourses);
+            displayCourses(filteredCourses);
+        });
+    });
+
+    displayCourses(allCourses); // By default show all courses from scratch
+}
+
+// Initialize
+setupFilters();
