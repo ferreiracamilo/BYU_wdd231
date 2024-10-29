5d87a5bc8bc3e54e22f2e362b279f6ae98de12b3
ferreiracamilo <camiloferreira2009@hotmail.com>
2024-10-29 20:30:10 -0300

    push initial skeleton to hltv site

---
+++ hltv/scripts/menuToggle.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const menuToggle = document.getElementById("menuToggle");
+    const navbar = document.getElementById("navbar");
+
+    menuToggle.addEventListener("click", function() {
+        navbar.classList.toggle("active");
+        menuToggle.classList.toggle("active");
+    });
+});
