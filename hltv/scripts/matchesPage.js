a90d351c92003da76e4d71a088901aa6b0b23178
ferreiracamilo <camiloferreira2009@hotmail.com>
2024-10-30 10:45:57 -0300

    update matches to show dynamycally content

--- hltv/scripts/matchesPage.js
+++ hltv/scripts/matchesPage.js
@@ -8,14 +8,15 @@ document.addEventListener("DOMContentLoaded", () => {
                 const article = document.createElement('article');
 
                 article.innerHTML = `
-                    <a href="./match-sample.html" data-match-id="${match.id}">
+                    <a href="./match-sample.html" data-match-id="${match.matchId}">
                         <div class="match">
-                            <img class="team-logo" src="https://img-cdn.hltv.org/teamlogo/g1gnqFhoa0FrcbRHXRAToF.png?ixlib=java-2.1.0&w=50&s=54d3d3762492bd158c053de080910c5f" alt="Team 1 Logo">
-                            <p class="score">2 - 0</p>
-                            <img class="team-logo" src="https://img-cdn.hltv.org/teamlogo/c27JiTOfpruzQaf8emu_E_.png?ixlib=java-2.1.0&w=50&s=b0c670f53aaf12df68c693515eecdce5" alt="Team 2 Logo">
+                            <img class="team-logo" src="${match.teams[0].logo}" alt="${match.teams[0].name} Team Logo">
+                            <p class="score">${match.teams[0].result} - ${match.teams[1].result}</p>
+                            <img class="team-logo" src="${match.teams[1].logo}" alt="${match.teams[1].name} Team Logo">
                         </div>
                     </a>
                 `;
+
                 articlesContainer.appendChild(article);
             });
         })
@@ -23,15 +24,3 @@ document.addEventListener("DOMContentLoaded", () => {
             console.error('Error fetching the team data:', error);
         });
 });
-
-/*
-<article>
-    <a href="#" data-match-id="123131">
-        <div class="match">
-            <img class="team-logo" src="https://img-cdn.hltv.org/teamlogo/g1gnqFhoa0FrcbRHXRAToF.png?ixlib=java-2.1.0&w=50&s=54d3d3762492bd158c053de080910c5f" alt="Team 1 Logo">
-            <p class="score">2 - 0</p>
-            <img class="team-logo" src="https://img-cdn.hltv.org/teamlogo/c27JiTOfpruzQaf8emu_E_.png?ixlib=java-2.1.0&w=50&s=b0c670f53aaf12df68c693515eecdce5" alt="Team 2 Logo">
-        </div>
-    </a>
-</article>
-*/
